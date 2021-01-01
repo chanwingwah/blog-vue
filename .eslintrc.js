dec17771adfaca604ac28936f7486c7a1c82baaa
chenyh <917029221@qq.com>
2021-01-02 03:21:52 +0800

    chore: 添加自定义ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,20 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // 标签需要自闭合
+    "vue/html-self-closing": ["error", {
+    "html": {
+      // 类似 img 使用 />
+      "void": "always",
+      // 常规html元素绝不自闭合，比如 <p> 不能写成 <p />
+      "normal": "never",
+      "component": "always"
+      },
+    }],
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }]
   }
 };
