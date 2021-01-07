f15a07e02d10e05bb9ce8f90f61a2ac45edfd652
chenyh <917029221@qq.com>
2021-01-08 07:47:57 +0800

    chore:开启gzip

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 ("use strict");
 const webpack = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
+const DEV = process.env.NODE_ENV !== "production";
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
@@ -48,6 +51,18 @@ module.exports = {
       args[0].title = "陈永华的博客";
       return args;
     });
+    if (!DEV) {
+      config.plugin("compressionPlugin").use(
+        new CompressionPlugin({
+          filename: "[path].gz[query]",
+          algorithm: "gzip",
+          test: productionGzipExtensions, //所有匹配此{RegExp}的资产都会被处理
+          threshold: 512, // 只处理大于此大小的资产。以字节为单位
+          minRatio: 0.8, //只有压缩好这个比率的资产才能被处理
+          deleteOriginalAssets: false //是否删除原资源
+        })
+      );
+    }
   },
   configureWebpack: {
     plugins: [
